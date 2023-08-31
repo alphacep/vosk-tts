3d32580ac7ac096b4c911e7199f310a832b5ee4a
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-08-31 23:42:36 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "rb") as fh:
 
 setuptools.setup(
     name="vosk-tts",
-    version="0.3.48",
+    version="0.3.49",
     author="Alpha Cephei Inc",
     author_email="contact@alphacephei.com",
     description="Offline text to speech synthesis",
